e0a1bf7906d4f7024bcc90390e4b1f40edc69abf
Quentin <villegentequentin@gmail.com>
2026-05-07 11:50:07 +0200

    meta viewport

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 
 <head>
     <meta charset="UTF-8">
-    <title></title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
 </head>
 
 <body>
