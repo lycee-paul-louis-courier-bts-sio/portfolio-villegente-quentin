6eb129cb3bac50c0c6b94d0628b340c11e2223db
Quentin <villegentequentin@gmail.com>
2026-05-07 12:10:04 +0200

    test display

--- vues/v-accueil.php
+++ vues/v-accueil.php
@@ -16,7 +16,7 @@
         <div class="card text-white text-center pt-5" style="background-color: #02332E">
             <div class="row">
                 <div class=" col-lg-6 col-md-6" col-sm-12>
-                    <p>Je m'appelle Quentin Villegente et je suis étudiant en BTS SIO spécialité
+                    <p class="display-6">Je m'appelle Quentin Villegente et je suis étudiant en BTS SIO spécialité
                         SLAM (Solution Logiciels et Applications Métiers) au lycée Paul Louis Courier.
                         Après avoir fait un Baccalauréat Général avec spécialité Mathématiques et NSI (Numérique et Science de l'Informatique),
                         le BTS SIO spécialité SLAM me semblait être la suite logique de mon parcours en développement.</p>
